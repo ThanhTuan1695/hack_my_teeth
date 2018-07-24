9ccba1138e4d055b045cb69f1ae93b825241a8dc
Tuan Thanh Tran <tuanthanhtran@Tuans-MacBook-Pro.local>
2018-07-24 17:36:57 +0700

    aaa

--- src/main/java/com/mgmsec/HackMyTeeth/HackMyTeeth/controllers/DentistController.java
+++ src/main/java/com/mgmsec/HackMyTeeth/HackMyTeeth/controllers/DentistController.java
@@ -1,17 +1,19 @@
 package com.mgmsec.HackMyTeeth.HackMyTeeth.controllers;
 
+import com.mgmsec.HackMyTeeth.HackMyTeeth.model.Session;
 import com.mgmsec.HackMyTeeth.HackMyTeeth.model.User;
+import com.mgmsec.HackMyTeeth.HackMyTeeth.service.SessionService;
 import com.mgmsec.HackMyTeeth.HackMyTeeth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -19,6 +21,9 @@ import java.util.List;
 public class DentistController {
     @Autowired
     UserService userService;
+
+    @Autowired
+    SessionService sessService;
 
     @Autowired
     private Environment environment;
@@ -29,16 +34,32 @@ public class DentistController {
     @RequestMapping(value = "/dentist", method = RequestMethod.GET)
     public ModelAndView dentistPage(HttpServletRequest request, Model model) {
         ModelAndView modelAndView = new ModelAndView();
-        String slqi = environment.getProperty("security.SQLI");
-        System.out.println(slqi);
-        String id = request.getParameter("id");
-        if (id == null) {
-            return new ModelAndView("redirect:/home");
+        Cookie loginCookie = sessService.checkLoginCookie(request);
+        System.out.println(loginCookie);
+        if(loginCookie != null) {
+            System.out.println("Login Cookie is: " +loginCookie.getValue());
+
+            Session sessions = sessService.findBySession(loginCookie.getValue());
+            if (sessions != null) {
+                String slqi = environment.getProperty("security.SQLI");
+                System.out.println(slqi);
+                String id = request.getParameter("id");
+                if (id == null) {
+                    return new ModelAndView("redirect:/home");
+                }
+                User dentist = userService.getDentistById(id, sqlinjection);
+                System.out.println(userService.getDentistById(id, sqlinjection));
+                modelAndView.addObject("dentist", dentist);
+                modelAndView.setViewName("dentist");
+            }
+            else {
+                return new ModelAndView("redirect: /login");
+            }
         }
-        User dentist = userService.getDentistById(id, sqlinjection);
-        System.out.println(userService.getDentistById(id, sqlinjection));
-        modelAndView.addObject("dentist", dentist);
-        modelAndView.setViewName("dentist");
+        else {
+            return new ModelAndView("redirect: /login");
+        }
+
         return modelAndView;
     }
 
